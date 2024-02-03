f6c342d776b74bc96fd51c5115b562e4ea0948a0
Travis Wright <tw277064@gmail.com>
2024-02-02 19:39:04 -0600

    This is intake code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,29 +5,20 @@ import com.revrobotics.CANSparkMax;
 
 
 public class Intake {
-    private CANSparkMax mot1;   
-    private CANSparkMax mot2;
+    private CANSparkMax motor1;   
+    private CANSparkMax motor2;
 
-    
-
-    public Intake(int id1, int id2){
-        mot1 = new CANSparkMax(id1, CANSparkLowLevel.MotorType.kBrushless);
-        mot2 = new CANSparkMax(id2, CANSparkLowLevel.MotorType.kBrushless);
+    public Intake(){
+        motor1 = new CANSparkMax(12, CANSparkLowLevel.MotorType.kBrushless);
+        motor2 = new CANSparkMax(15, CANSparkLowLevel.MotorType.kBrushless);
     }
 
     public void start(double speed){
-        mot1.set(speed);
-        mot2.set(-speed);
+        motor1.set(speed);
+        motor2.set(-speed);
     }
     public void stop(){
-        mot1.set(0);
-        mot2.set(0);
+        motor1.set(0);
+        motor2.set(0);
     }
-
-
-
-
-
-
-
 }
