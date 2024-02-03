883321564bdf10966bf0c73c41425bc5dcc58031
Travis Wright <tw277064@gmail.com>
2024-02-03 12:31:44 -0600

    Travvys Intake code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,14 +3,29 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 
 public class Intake {
-    private CANSparkMax motor1;   
+   private CANSparkMax motor1;   
     private CANSparkMax motor2;
+    private DigitalInput Intake;
+
 
     public Intake(){
         motor1 = new CANSparkMax(12, CANSparkLowLevel.MotorType.kBrushless);
         motor2 = new CANSparkMax(15, CANSparkLowLevel.MotorType.kBrushless);
+        Intake = new DigitalInput(0);
+        
+    }
+
+    public void rununtilnote(){
+        if (Intake.get()) {
+            stop();
+        }
+        else {
+            start(.5);
+        }
     }
 
     public void start(double speed){
@@ -22,3 +37,17 @@ public class Intake {
         motor2.set(0);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
