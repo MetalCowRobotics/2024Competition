a04c32be49331971c3b81e50345ffe62b57e8827
kosjxsaj <68974105+ANProgramer@users.noreply.github.com>
2024-02-15 17:46:25 -0700

    Robot Intake
    Subsystem of the intake

--- src/main/java/frc/robot/subsystems/Robot.java
+++ src/main/java/frc/robot/subsystems/Robot.java
@@ -2,13 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.CTREConfigs;
 import frc.robot.subsystems.*;
 
 /*
